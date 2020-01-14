7f0cf692b94d1453bc3b9f2c82dc80722bdc4343
Mark Guindi <markguindi1@gmail.com>
2020-01-14 15:00:52 -0500

    Update app.component.ts

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Todo App';
+  todos = [
+    {
+      text: 'Learn Angular',
+      done: false,
+      id: 0
+    }
+  ];
+
+  newTodoId = 1;
+
+  ngOnInit(): void {
+
+  }
+
+  addTodo(newTodoText: string): void {
+    this.todos.push({
+      text: newTodoText,
+      done: false,
+      id: this.newTodoId
+    });
+    this.newTodoId++;
+  }
+
+  deleteTodo(deletedTodo): void {
+    this.todos = this.todos.filter(todo => todo.id !== deletedTodo.id);
+  }
+
+  clearDone(): void {
+    this.todos = this.todos.filter(todo => !todo.done);
+  }
+
 }
