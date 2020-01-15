0dcd9f9f1aa7781b587a1654a1d73818e0b93039
markguindi1 <markguindi1@gmail.com>
2020-01-15 12:34:09 -0500

    Use service for getting & deleting todos

--- frontend/todo/src/app/app.component.ts
+++ frontend/todo/src/app/app.component.ts
@@ -9,7 +9,6 @@ import { TodoService } from './todo.service';
 export class AppComponent implements OnInit {
   title = 'Todo App';
   todos = [];
-  newTodoId = 1;
 
   constructor(private todoService: TodoService) {}
 
@@ -18,24 +17,39 @@ export class AppComponent implements OnInit {
   }
 
   fetchTodos(): void {
-    this.todoService.getTodos().subscribe(todos => this.todos = todos);
+    this.todoService.getTodos().subscribe(todos => this.todos = todos.concat());
   }
 
   addTodo(newTodoText: string): void {
-    this.todos.push({
-      text: newTodoText,
-      done: false,
-      id: this.newTodoId
+    if (!newTodoText) {
+      return;
+    }
+    // Call service to add Todo, and push in subscribe()
+    this.todoService.addTodo(this.capitalize(newTodoText)).subscribe(todo => {
+      console.log(this.todoService.TODOS);
+      console.log(this.todos);
+      this.todos.push(todo);
     });
-    this.newTodoId++;
+  }
+
+  capitalize(str): string {
+    if (typeof str !== 'string') {
+      return '';
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
   deleteTodo(deletedTodo): void {
     this.todos = this.todos.filter(todo => todo.id !== deletedTodo.id);
+    this.todoService.deleteTodo(deletedTodo.id).subscribe();
   }
 
   clearDone(): void {
+    const finishedTodos = this.todos.filter(todo => todo.done);
     this.todos = this.todos.filter(todo => !todo.done);
+    finishedTodos.forEach(todo => {
+      this.todoService.deleteTodo(todo.id).subscribe();
+    });
   }
 
 }
